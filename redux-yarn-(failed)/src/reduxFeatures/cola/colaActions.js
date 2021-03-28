8fe290d44918ff4ef86fee0c5b504ec1c442a694
jecky hindrawan <jecky@coconutlab.io>
2021-03-28 11:35:57 +0700

    redux

--- redux-yarn-(failed)/src/reduxFeatures/cola/colaActions.js
+++
@@ -1,7 +0,0 @@
-import {BUY_COLA} from './colaTypes';
-
-export const buyCola = () => {
-  return {
-    type: BUY_COLA,
-  };
-};
--- redux-yarn-(failed)/src/reduxFeatures/index.js
+++
@@ -1,4 +0,0 @@
-export {buyCake} from './cakes/cakeActions';
-export {buyIceCream} from './iceCream/iceCreamActions';
-export {buyCola} from './cola/colaActions';
-export * from './user/userActions';
